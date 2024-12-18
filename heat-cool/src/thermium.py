636b49987442f8b5dc91e0145e14d7bf444db048
jrhillery <john.hillery2@gmail.com>
2024-12-18 09:23:59 -0500

    Add sensor details

--- heat-cool/src/thermium.py
+++ heat-cool/src/thermium.py
@@ -7,11 +7,13 @@ from abc import ABC, abstractmethod
 from argparse import ArgumentParser, Namespace
 from contextlib import AsyncExitStack
 from platform import node
+from urllib.parse import urlsplit, urlunsplit
 
 from aiohttp import ClientSession
 from nexia.const import BRAND_ASAIR
 from nexia.home import NexiaHome
 from nexia.thermostat import NexiaThermostat
+from nexia.zone import NexiaThermostatZone
 from wakepy import keep
 
 from util import Configure, PersistentData
@@ -87,8 +89,9 @@ class NexiaProc(ABC):
 
         self.nexiaHome = NexiaHome(session, **accessToken, device_name=node(),
                                    brand=BRAND_ASAIR, state_file=stateFile)
+        self.rootUrlParts = urlsplit(self.nexiaHome.root_url)
         del accessToken
-    # end __init__(ClientSession)
+    # end __init__(PersistentData, ClientSession)
 
     @abstractmethod
     async def process(self) -> None:
@@ -96,12 +99,55 @@ class NexiaProc(ABC):
         pass
     # end process()
 
+    @staticmethod
+    async def sensorData(therm: NexiaThermostat) -> str:
+        roomIqSensors = therm._get_thermostat_features_key("room_iq_sensors")
+        sensorDetails: list[str] = [
+            ("," if sensor["type"] == "thermostat" else f"{sensor["name"]}:")
+            + f" {sensor["temperature"]}\u00B0"
+              f" humidity {sensor["humidity"]}%"
+            for sensor in roomIqSensors["sensors"]]
+
+        return "; ".join(sensorDetails)
+    # end sensorData(NexiaThermostat)
+
     async def login(self) -> bool:
         await self.nexiaHome.login()
         await self.nexiaHome.update()
 
+        for therm in self.nexiaHome.thermostats:
+            logging.debug(f"{therm.get_name()} at login{await self.sensorData(therm)}")
+
         return self.nexiaHome.thermostats is not None
     # end login()
+
+    async def updateSensorData(self, zone: NexiaThermostatZone) -> None:
+        url = zone.API_MOBILE_ZONE_URL.format(
+            end_point="request_current_sensor_state", zone_id=zone.zone_id)
+        response = await self.nexiaHome.post_url(url, {})
+        pollingPathParts = urlsplit((await response.json())["result"]["polling_path"])
+        pollingPath = str(urlunsplit(pollingPathParts._replace(
+            scheme=self.rootUrlParts.scheme, netloc=self.rootUrlParts.netloc)))
+        status: str | None = None
+
+        while status is None:
+            response = await self.nexiaHome._get_url(pollingPath)
+            data = (await response.read()).strip()
+
+            if data == b"null":
+                await asyncio.sleep(0.8)
+            else:
+                status = json.loads(data)["status"]
+        # end while waiting for status
+
+        if status != "success":
+            logging.debug(f"Unexpected status [{status}] from request current sensor state")
+    # end updateSensorData(NexiaThermostatZone)
+
+    async def updateThermostatSensorData(self, therm: NexiaThermostat) -> None:
+        for zone in therm.zones:
+            await self.updateSensorData(zone)
+    # end updateThermostatSensorData(NexiaThermostat)
 
     @staticmethod
     def auxOnOff(therm: NexiaThermostat) -> str:
@@ -123,13 +169,16 @@ class AuxHeatEnabler(NexiaProc):
         for therm in self.nexiaHome.thermostats:
             auxHeatOn: bool = therm.is_emergency_heat_active()
             self.persistData.setVal(self.PRIOR_AUX_STATE, therm.get_device_id(), auxHeatOn)
+            await self.updateThermostatSensorData(therm)
 
             if auxHeatOn:
-                logging.info(f"{therm.get_name()} auxiliary heat was already on")
+                await self.nexiaHome.update()
+                logging.info(f"{therm.get_name()} auxiliary heat was already on"
+                             f"{await self.sensorData(therm)}")
             else:
                 await therm.set_emergency_heat(True)
                 logging.info(f"{therm.get_name()} auxiliary heat changed from"
-                             f" off to {self.auxOnOff(therm)}")
+                             f" off to {self.auxOnOff(therm)}{await self.sensorData(therm)}")
         # end for each thermostat
     # end process()
 
@@ -150,13 +199,17 @@ class AuxHeatRestorer(NexiaProc):
                                                                 therm.get_device_id())
             if priorAuxHeat is None:
                 priorAuxHeat = False
+            await self.updateThermostatSensorData(therm)
 
             if auxHeatOn == priorAuxHeat:
-                logging.info(f"{therm.get_name()} auxiliary heat was already {self.auxOnOff(therm)}")
+                await self.nexiaHome.update()
+                logging.info(f"{therm.get_name()} auxiliary heat was already"
+                             f" {self.auxOnOff(therm)}{await self.sensorData(therm)}")
             else:
                 await therm.set_emergency_heat(priorAuxHeat)
                 logging.info(f"{therm.get_name()} auxiliary heat changed from"
-                             f" {"on" if auxHeatOn else "off"} to {self.auxOnOff(therm)}")
+                             f" {"on" if auxHeatOn else "off"} to {self.auxOnOff(therm)}"
+                             f"{await self.sensorData(therm)}")
         # end for each thermostat
     # end process()
 
@@ -172,7 +225,10 @@ class StatusPresenter(NexiaProc):
             return
 
         for therm in self.nexiaHome.thermostats:
-            logging.info(f"{therm.get_name()} auxiliary heat is {self.auxOnOff(therm)}")
+            await self.updateThermostatSensorData(therm)
+            await self.nexiaHome.update()
+            logging.info(f"{therm.get_name()} auxiliary heat is {self.auxOnOff(therm)}"
+                         f"{await self.sensorData(therm)}")
     # end process()
 
 # end class StatusPresenter
